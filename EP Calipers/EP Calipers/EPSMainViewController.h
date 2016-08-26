77cf02e841550bdccab351c37edd85cfa36f07a0
David Mann <mannd@epstudiossoftware.com>
2016-08-25 19:26:32 -0600

    Implement More Adjust menu
    Micro-tweaking (0.1 degree rotation) is implemented in a "More" menu
    which is a submenu of the Adjust menu.  All the adjust items can't fit
    on a toolbar if using a small phone (like the 4S).  Flip is rarely used
    and also moved to the More menu.  Exiting the More menu brings one back
    to the Adjust menu.  Reset remains on the More menu, perhaps it needs to
    be on both menus?

--- EP Calipers/EP Calipers/EPSMainViewController.h
+++ EP Calipers/EP Calipers/EPSMainViewController.h
@@ -29,6 +29,7 @@
 @property (strong, nonatomic) NSArray *photoMenuItems;
 @property (strong, nonatomic) NSArray *calipersMenuItems;
 @property (strong, nonatomic) NSArray *adjustImageMenuItems;
+@property (strong, nonatomic) NSArray *moreAdjustImageMenuItems;
 @property (strong, nonatomic) NSArray *addCalipersMenuItems;
 @property (strong, nonatomic) NSArray *calibrateMenuItems;
 @property (strong, nonatomic) NSArray *qtcStep1MenuItems;
